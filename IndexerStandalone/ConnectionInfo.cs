7ed15dda6634dccd1957bb01a78c024eca0f270c
GorudoYami <gorudoyami@gmail.com>
2021-07-15 17:09:47 +0200

    Removed standalone versions

--- IndexerStandalone/ConnectionInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace IndexerStandalone {
-    internal class ConnectionInfo {
-        public string Server { get; set; }
-        public string Port { get; set; }
-        public string User { get; set; }
-        public string Password { get; set; }
-        public string Database { get; set; }
-    }
-}
